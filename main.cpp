a059cd65a5f2c09d3163f46d7a65553d49bdaef4
MutterGott <bogdan.bogotov123123@gmail.com>
2021-04-05 01:52:37 +0300

    Change main.cpp for Laba4

--- main.cpp
+++ main.cpp
@@ -11,9 +11,8 @@ int main() {
     Client object = Client(strdup("Cantik"), strdup("Moscow"), 23);
     list.add(object);
     list.add(strdup("Bogdan"), strdup("Moscow"), 25);
-    list.addKey();
-    list.deleteClient(0);
-    cout << "\nList: " << list.checkList() << endl;
-    list.showList();
+    list[list.find(strdup("fio"), strdup("Bogdan"))].ShowClient();
+    list[list.find(strdup("adress"), strdup("Moscow"))].ShowClient();
+    list[list.find(25)].ShowClient();
     system("pause");
 }
