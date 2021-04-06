706de570650780d4bfbba9c5538bb28bcdafb039
MutterGott <bogdan.bogotov123123@gmail.com>
2021-04-06 23:16:05 +0300

    Added show find(), sortList()

--- main.cpp
+++ main.cpp
@@ -8,11 +8,18 @@ using namespace std;
 int main() {
     setlocale(0, "");
     ListClients list = ListClients(3);
-    Client object = Client(strdup("Cantik"), strdup("Moscow"), 23);
+    Client object = Client(_strdup("Cantik"), _strdup("Boscow"), 23);
     list.add(object);
-    list.add(strdup("Bogdan"), strdup("Moscow"), 25);
-    list[list.find(strdup("fio"), strdup("Bogdan"))].ShowClient();
-    list[list.find(strdup("adress"), strdup("Moscow"))].ShowClient();
-    list[list.find(25)].ShowClient();
+    list.add(_strdup("Bogdan"), _strdup("Moscow"), 40);
+    list[list.find(1, _strdup("Bogdan"))].ShowClient();
+    list[list.find(2, _strdup("Moscow"))].ShowClient();
+    cout << list.find(25) << endl;
+    list.showList();
+    list.sortList(3);
+    list.showList();
+    list.sortList(2);
+    list.showList();
+    list.sortList(1);
+    list.showList();
     system("pause");
 }
